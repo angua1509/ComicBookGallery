7dcecbef3049b62e3a810ffe39f170a2ff87dc31
angua1509 <47885490+angua1509@users.noreply.github.com>
2021-01-18 12:26:55 +0100

    added ComicBooksController

---
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+
+    }
+}
