79ece4efaeb4c54f40bcf9e0be3ee594d5b57026
angua1509 <47885490+angua1509@users.noreply.github.com>
2021-01-18 13:08:10 +0100

    added material from Nackademi course for uderstanding

---
+++ src/ComicBookGallery/ComicBookGallery/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComicBookGallery.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Message is variable name
+            ViewBag.Message = "Detta är tet som kommer från controllern";
+            return View();
+
+            // calls a specified View
+            // return View("AlternativtIndex");
+        }
+    }
+}
--- src/ComicBookGallery/ComicBookGallery/Startup.cs
+++ src/ComicBookGallery/ComicBookGallery/Startup.cs
@@ -16,25 +16,23 @@ namespace ComicBookGallery
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(param => param.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            // creates a start route till Home/Index
+            //app.UseMvcWithDefaultRoute();
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            app.UseMvc(routes =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                routes.MapRoute(
+                name: "Default",
+                template: "{Controller=Home}/{action=Index}"
+                );
             });
+            
         }
     }
 }
